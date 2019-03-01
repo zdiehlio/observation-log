d72c3467165c93a9fe7fc0a9105cffa52293eb6a
Zachary Diehl <zdiehlio@Zacharys-MacBook-Air.local>
2019-03-01 18:29:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    newAnimal: [],
+    currentAnimal: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({currentAnimal: event.target.value})
+    console.log(this.state.currentAnimal)
+  }
+
+  addAnimal = (event) => {
+    event.preventDefault()
+    this.setState({newAnimal: this.state.newAnimal.push(this.state.currentAnimal)})
+  }
+  
+  newAnimal = this.state.newAnimal.map(val => {
+    return(
+      <tr>
+        <td>{val}</td>
+      </tr>
+    )
+  })
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          NA Cetacean Observation Log
         </header>
+        <div>
+          <form>
+            <input value={this.state.currentAnimal} placeholder='Animal' name='animal' onChange={this.handleChange}></input>
+            <button onClick={this.addAnimal}>Add</button>
+          </form>
+          <table>
+            <tr>
+              <th>Time</th>
+              <th>Animal(s) Involved</th>
+              <th>Fecal</th>
+              <th>Urine</th>
+              <th>Regurgitation</th>
+              <th>Comments</th>
+            </tr>
+            {this.newAnimal}
+          </table>
+        </div>
       </div>
     );
   }
